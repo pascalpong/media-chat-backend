3771c0e23dc732d04d192d346e2b29953c461a9a
PCD <pascalpcd@bestpicktechnology.com>
2024-03-01 15:32:20 +0700

    ss

--- src/controllers/messageController/index.ts
+++ src/controllers/messageController/index.ts
@@ -28,12 +28,10 @@ export const chatRoomMessages = async (req: Request, res: Response) => {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10; // Set limit here or a default value
         const skip = (page - 1) * limit;
-
         const messages = await Message.find({roomId})
             .skip(skip)
             .limit(limit)
             .sort({ createdAt: -1 }); // Sort by createdAt in descending order
-
         return res.json(messages);
     } catch (error) {
         console.log(error);
